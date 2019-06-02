5ae4b9e1bb62882e47515f3fd92be851fade8b20
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-01 21:52:44 -0400

    Create adresse.java

---
+++ Grp1Library/adresse/adresse.java
@@ -0,0 +1,13 @@
+package adresse;
+
+public class Address{
+    public int streetNum;
+    public String city;
+    public String country;
+    
+ public Address(int street, String c, String coun){
+    this.streetNum=street;
+    this.city =c;
+    this.country = coun;
+   }
+}
