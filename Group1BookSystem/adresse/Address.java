f750c297a73a174579ad330cdfa8b677ea22008e
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-09 16:36:44 -0400

    Create Address.java

---
+++ Group1BookSystem/adresse/Address.java
@@ -0,0 +1,15 @@
+// June 1st. 2019
+//@Author Rodrigue.
+
+package adresse;
+
+public class Address{
+    public int streetNum;
+    public String city;
+    public String country;
+   public Address(int street, String c, String coun){
+       this.streetNum=street;
+       this.city =c;
+       this.country = coun;
+   }
+}
