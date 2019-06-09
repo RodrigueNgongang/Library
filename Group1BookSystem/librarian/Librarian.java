e96bac6fa5698e988ce6042afa17548d88790513
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-09 16:29:09 -0400

    Create Librarian.java

---
+++ Group1BookSystem/librarian/Librarian.java
@@ -0,0 +1,41 @@
+package librarian;
+import User.*;
+import adresse.*;
+import Book.*;
+
+public class Librarian extends User {
+    public int libId;
+    public Address libAdress;
+    public Librarian(String login, String pwd, String func, int id, Address addr){
+	//System.out.println("Constructor of Child");
+       super(login, pwd, func); 
+       libId = id;
+       libAdress = addr;
+   }
+
+   public String getUsername() {
+        return super.getUsername() +" the Librarian";
+    }
+   
+   public String getRole() {
+        return super.getRole();
+     }
+  
+   public String getPassword() {
+        return super.getPassword();
+    }
+
+   public int getlibId() {
+        return libId;
+    }
+    public void setlibId(int id) {
+        this.libId = id;
+    }
+
+    public Address getlibAdress() {
+        return libAdress;
+    }
+    public void setlibAdress(Address addr) {
+        this.libAdress = addr;
+    }
+}
