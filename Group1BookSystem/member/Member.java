b817dac233f8118e3f1d9e9caf7db388bd1905e8
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-09 16:21:33 -0400

    Create Member.java

---
+++ Group1BookSystem/member/Member.java
@@ -0,0 +1,41 @@
+package member;
+import User.*;
+import adresse.*;
+import Book.*;
+
+public class Member extends User {
+    public int membId;
+    public Address membAdress;
+    
+    public Member(String login, String pwd, String func, int id, Address addr){
+       super(login, pwd, func); 
+       membId = id;
+       membAdress = addr;
+   }
+
+   public String getUsername() {
+        return super.getUsername() +" the Member";
+    }
+   
+   public String getRole() {
+        return super.getRole();
+     }
+  
+   public String getPassword() {
+        return super.getPassword();
+    }
+
+   public int getMembId() {
+        return membId;
+    }
+    public void setMembId(int id) {
+        this.membId = id;
+    }
+
+    public Address getMembAdress() {
+        return membAdress;
+    }
+    public void setMembAdress(Address addr) {
+        this.membAdress = addr;
+    }
+}
