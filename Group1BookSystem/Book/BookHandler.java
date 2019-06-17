1efd7e2db9c306efeae1ea36866e7447a41305ef
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-16 21:00:35 -0400

    Update BookHandler.java

--- Group1BookSystem/Book/BookHandler.java
+++ Group1BookSystem/Book/BookHandler.java
@@ -2,99 +2,131 @@
 //@Author Rodrigue.
 
 package Book;
-import java.util.Scanner;
+import member.*;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator; 
+import java.time.LocalDate; 
 
-public class BookHandler extends Book{
-   //Book[] books = new Book[10];
-   //public ArrayList<Book> books = new ArrayList<Book>();                
-   //public int count = 0;
-
-   public BookHandler (String titre, String auteur, String id){
-       super(titre,auteur,id);
-   }
-   public String getTitle(){
-        return super.getTitle();
-    }
-    
-    public String getAuthor(){
-        return super.getAuthor();
-    }
-    
-    public String getIsbn(){
-        return super.getIsbn();
-    }
-    
-    public void setTitle(String title){
-        this.title=title;
-    }
-    
-    public void setAuthor(String author){
-       this.author=author;
-    }
+public class BookHandler {
    
-    public void setIsbn(String id){
-        this.isbn=id;}
+   final static int maxBook = 10; 
+   static int numBooks = 0;  
+   private int count = 0;                 
+   private String title;
+   private String author;
+   private String isbn; 
+   private LocalDate dueDate; 
+   public ArrayList<Book> books = new ArrayList<Book>();
+   public HashMap<Member, Book> issuedBook = new HashMap<Member, Book>();
+   
+   public BookHandler (String titre, String auteur, String id){
+      this.title = titre;
+      this.author = auteur;
+      this.isbn = id; 
+      this.dueDate = null;   
+      }
 
-   public Boolean adddBook(Book book) {
-     /*   if (book.getIsbn() != 0) {
-               if (count < 100)
-                  this.book[count++] = book;
-                  else
-                  System.out.println("Array is full. Can't add more.");
-                  }*/
-                  return false;
-                  }
+   public String getTitle(){
+        return title;}
+    
+   public String getAuthor(){
+        return author;}
+    
+   public String getIsbn(){
+        return isbn;}
+    
+   public void setTitle(String title){
+        this.title=title;}
+    
+   public void setAuthor(String author){
+       this.author=author;}
+   
+   public void setIsbn(String id){
+       this.isbn=id;}
 
-public ArrayList<Book> books = new ArrayList<Book>();
-public int count = 0;
-public void addNewBook(Book newbook){  
+  public LocalDate getDueDate(){
+      return dueDate;}
+
+  public void setDueDate(LocalDate dueDate){
+     this.dueDate = dueDate;}
+
+   public void base() {
+   Book bb = new Book("Starting Out with Java", "Tony Gaddis", "0133957055", false); 
+   books.add(bb);
+   }
+
+   public void addNewBook(Book newbook){  
       Book newBook = newbook;
-      //("author", "title", "124EBN125");
-      //library[numBooks]=new Book("test","test","test","test",0);
-      addBook(newBook); }
-
+      addBook(newBook);   
+      }
+   
    public void addBook(Book newBook) {
-       System.out.println("You wish to add a book."); 
-       books.add(newBook);
-       //for (Book b : books) {
-       //System.out.println(b);
-       System.out.println(newBook.title + " of " + newBook.author + " Added");
-       count++;
-       System.out.println("Total books in the library: "+count);          
-       //numBooks++;
-         }
+     System.out.println("You wish to add a book.");
+     if (numBooks < maxBook){ 
+     books.add(newBook);
+     numBooks = numBooks + 1 ;
+     }
+     System.out.println(newBook.getTitle() + " of " + newBook.getAuthor() + " Added");
+     count = numBooks;
+     System.out.println("Total books in the library: "+ count);
+     } 
 
    public void delBook(Book newBook) {
        Book book = newBook;
        Iterator iter = books.iterator();
        System.out.println("You wish to delete a book.");
-      /* while(iter.hasNext()) {
-            iter.next();
-            books.remove(book);
-        } */
-      if(count>0){
+       if(count>0){
        books.remove(book);
-       System.out.println(book.author + " Removed");
+       System.out.println(book.getTitle() + " of " + book.getAuthor() + " Removed");
        count--;
+       System.out.println("Total books in the library: "+count);
        }else{
-       System.out.println("No Book available");      
-       //numBooks++; 
+       System.out.println("No Book available");       
        System.out.println("Total books in the library: "+count); 
-        } }
-
- public void searchBook(String title){
-     //ArrayList<Book> found = new ArrayList<Book>();
+        } 
+      } 
+   public void searchBook(String title){
      for(Book bk : books) {
           if(bk.getTitle().equals(title)){
-          System.out.println("book found.");            
+          System.out.println("Book found.");            
           System.out.println("Author : " + bk.getAuthor() + ", Title : " + bk.getTitle() + ", isbn: " + bk.getIsbn());
           break;  
           }else{
                System.out.println("book was not found."); 
                }
           } 
-      }
+        }
+   public void requestBook(Member usr, String title, LocalDate checkoutDate){
+     System.out.println("You wish to request '"+title+"'");
+     base(); 
+     for(Book bk : books) {
+          if(bk.getTitle().equals(title)){
+              bk.setDueDate(checkoutDate.plusWeeks(3));
+              issuedBook.put(usr, bk);
+              System.out.println("\t Borrower Name     : " + usr.getUsername());
+              System.out.println("\t Borrower Adress   : " + usr.membAdress.getCountry() + "-" + usr.membAdress.getCity()); 
+              System.out.println("\t Book Informations : " + bk.getAuthor() + "-" + bk.getTitle());                    
+              System.out.println("\t Book due date     : " + bk.getDueDate() );
+              break; 
+           }else{
+            System.out.println("Book was not found in the system."); 
+            }
+          } 
+     }	
+   public void returnBook(String title){
+      System.out.println("You wish to return '"+title+"'");
+      base();
+      for(Book bk : books) {
+          if(bk.getTitle().equals(title)){
+             issuedBook.remove(bk);
+             System.out.println("--" + bk.getTitle() + " of " + bk.getAuthor() + " has been checked back in --");            
+             break;  
+            }else{
+             System.out.println("Book was not found in the system."); 
+             }
+          }
+   }
+
 }
