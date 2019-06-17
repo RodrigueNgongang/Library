f29318e32275d9ba50be747ea139c9fe79cb4017
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-16 20:42:20 -0400

    Update Rodrigue.java

--- Group1BookSystem/Rodrigue.java
+++ Group1BookSystem/Rodrigue.java
@@ -2,11 +2,13 @@
 //@Author Rodrigue.
 
 import User.*;
+import Book.*;
+import admin.*;
 import member.*;
 import adresse.*;
 import librarian.*;
-import Book.*;
 import BookHandler.*;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Rodrigue { 
@@ -14,20 +16,23 @@ public class Rodrigue {
 public static void main(String args[]){
 
         Address ad = new Address(12, "Douala", "Kmer");
-        Librarian usr = new Librarian("Rodrigue", "One", "Librarian", 111, ad);
-        Book Book1 = new Book("L'Urgence de la pensee", "Maurice Kamto", "124EBN125");
-        Book Book2 = new Book("Starting Out with Java", "Tony Gaddis", "0133957055");
+        Member usr = new Member ("Armand", "Two", "Member", 111, ad);
+        Admin adm = new Admin ("Stephens", "Three", "Admin", 123, ad);
+        Librarian lib = new Librarian ("Rodriguo", "Two", "Member", 111, ad);
+        Book Book1 = new Book("L'Urgence de la pensee", "Maurice Kamto", "124EBN125", false);
+        Book Book2 = new Book("Starting Out with Java", "Tony Gaddis", "0133957055", false);
         //BookHandler bh = new BookHandler();
-        //User usr = new User("Rodrigue", "One", "Librarian");
+        User rmlib = new User("Rodrigue", "One", "Librarian");
+        User nwlib = new User("Daphney", "Four", "Librarian");
         //boolean flag = lib1.login();
-        boolean flag = usr.login();
-        
+        boolean flag = adm.login();
+        //System.out.println(flag);
         Scanner keyboard = new Scanner(System.in); 
 
         if(!flag){
                 System.out.println("login or password is incorrect");
                 }else{
-                    if(usr.getRole()=="Librarian"){
+                    if(adm.getRole()=="Librarian"){
                      System.out.println("I'm Librarian");
                      char exit = 'N';
                      while (exit != 'Y') {
@@ -37,32 +42,33 @@ public static void main(String args[]){
                      System.out.println("\t3. Issue a book to a member.");
                      System.out.println("\t4. Remove a book from the system.");
                      System.out.println("\t5. Logout.");
-                     System.out.print("Enter your choice : ");
+                     System.out.print("Enter the number that applies: ");
                      int choice = keyboard.nextInt();
                      keyboard.nextLine();
+                     // Determine which command was selected.
                      switch (choice) {
-                     case 1: 
+                     case 1: // 1. Add a book to the system.
                      Book1.addNewBook(Book1);
                      Book2.addNewBook(Book2);
                      break;
-                     case 2: 
+                     case 2: // 2. Search a book from the system.
                      Book2.searchBook(Book2.getTitle());
                      break;
-                     case 3: 
+                     case 3: // 2. Issue a book to a member.
                      //Book1.IssueBook(Book1);
-                     case 4: 
+                     case 4: // 2. Search a book from the system.
                      Book1.delBook(Book1);
                      break;
-                     case 5: 
+                     case 5: // 5. logout the system.
                      usr.logout();
                      break;
-                     default: 
-                     System.out.println("That is not a valid choice.");
+                     default: // Invalid command
+                     System.out.println("That is not a valid command.");
                      }
                      System.out.print("Would you like to exit, Y or N: ");
                      exit = keyboard.nextLine().charAt(0);
                      }
-                     }else if(usr.getRole()=="Admin"){
+                     }else if(adm.getRole()=="Admin"){
                            System.out.println("I'm Admin");
                            char exit = 'N';
                            while (exit != 'Y') {
@@ -70,20 +76,21 @@ public static void main(String args[]){
                            System.out.println("\t1. Add a Librarian.");
                            System.out.println("\t2. Delete a Librarian.");
                            System.out.println("\t3. Logout.");
-                           System.out.print("Enter your choice : ");
+                           System.out.print("Enter the number that applies: ");
                            int choice = keyboard.nextInt();
                            keyboard.nextLine();
+                           // Determine which command was selected.
                            switch (choice) {
-                           case 1: 
-                           //addLib();
+                           case 1: // 1. Add a Librarian to the system.
+                           adm.AddLib(nwlib);
                            break;
-                           case 2: 
-                           //deleteLib();
+                           case 2: // 2. Delete a Librarian from the system.
+                           adm.DelLib(rmlib);
                            break;
-                           case 3: 
-                           //logout();
+                           case 3: // 3. logout the system.
+                           adm.logout();
                            break;
-                           default: 
+                           default: // Invalid command
                            System.out.println("That is not a valid command.");
                            }
                             System.out.print("Would you like to exit, Y or N: ");
@@ -97,26 +104,27 @@ public static void main(String args[]){
                              System.out.println("\t1. Request a book.");
                              System.out.println("\t2. Return a book.");
                              System.out.println("\t3. Logout.");
-                             System.out.print("Enter your choice: ");
+                             System.out.print("Enter the number that applies: ");
                              int choice = keyboard.nextInt();
-                             keyboard.nextLine();                            
+                             keyboard.nextLine();
                              switch (choice) {
-                             case 1: 
-                             //Book2.requestBook(Book);
+                             case 1: // 1. Request a book.
+                             Book2.requestBook(usr, Book2.getTitle(), LocalDate.of(2019, 06, 16));
                              break;
-                             case 2: 
-                             //Book2.returnBook(Book);
+                             case 2: // 2. Return a book.
+                             Book2.returnBook(Book2.getTitle());
                              break;
-                             case 3: 
-                             //logout();
+                             case 3: // 3. logout the system.
+                             usr.logout();
                              break;
-                             default: 
+                             default: // Invalid command
                              System.out.println("That is not a valid command.");
                              }
                              System.out.print("Would you like to exit, Y or N: ");
                              exit = keyboard.nextLine().charAt(0);
                              }
-                             }}
+                             }
+        }
 
    }
 }
