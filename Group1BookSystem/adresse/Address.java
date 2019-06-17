76d48a084ce40bc71882563024d4d278a8fd5212
RodrigueNgongang <42192863+RodrigueNgongang@users.noreply.github.com>
2019-06-16 20:49:38 -0400

    Update Address.java

--- Group1BookSystem/adresse/Address.java
+++ Group1BookSystem/adresse/Address.java
@@ -4,12 +4,37 @@
 package adresse;
 
 public class Address{
+    
     public int streetNum;
     public String city;
     public String country;
+    
    public Address(int street, String c, String coun){
        this.streetNum=street;
        this.city =c;
        this.country = coun;
-   }
+       }
+   public int getStreetNum(){
+      return streetNum;
+      }
+    
+   public String getCity(){
+      return city;
+      }
+    
+   public String getCountry(){
+      return country;
+      }
+    
+   public void setStreetNum(int streetNum){
+      this.streetNum=streetNum;
+      }
+    
+   public void setCity(String city){
+      this.city=city;
+      }
+   
+   public void setCountry(String country){
+      this.country=country;
+      }
 }
